99b9308fcecaf9eba4dd8a1bdfc9242667a722c6
搋兔 <naruto6404@hotmail.com>
2021-07-30 20:50:58 +0800

    update: fixed packages version number

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
-  purge: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue}'],
+  purge: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
