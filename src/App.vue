24884aaabca0f0f789ed3f5c53f315f505b4ab3d
搋兔 <naruto6404@hotmail.com>
2021-08-04 09:21:37 +0800

    fix typo, add powered-by image

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="text-center">
     <img class="mx-auto mt-12 mb-8" alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + Vit + Tailwind jit" />
+    <HelloWorld msg="Vite + Vue 3 + Tailwind jit" />
+    <img class="mx-auto my-8" alt="powered-by" src="./assets/powered-by-vitawind-bright.png">
   </div>
 </template>
 
